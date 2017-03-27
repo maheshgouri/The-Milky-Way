3949253eb6259e805d480ea67458471e91d60593
Mahesh Babu <Mahesh.Gourigari@searshc.com>
2017-03-27 15:29:25 -0500

    Planets list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,50 @@
 import { Component } from '@angular/core';
+import { FormsModule} from '@angular/forms'
+
+export class Planet {
+  orbit = number;
+  name = string;
+}
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+<h1>{{heading}}</h1>
+
+<h2>Our Planets</h2>
+<ul class="heroes">
+<li *ngFor="let planet of planets">
+<span class="badge">
+<label>{{planet.orbit}}</label></span>: {{planet.name}}
+</li>
+</ul>
+
+<h2>I am {{planet.name}}!</h2>
+<div><label>Orbit: </label>{{planet.orbit}}</div>
+<div>
+<label>Name: </label>
+<input [(ngModel)]="planet.name" placeholder="Name">
+</div>
+`
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  heading = 'The Milky Way';
+  planets = PLANETS;
+  planet: Planet = {
+    orbit: 3,
+    name: 'Earth'
+  };
+}
+const PLANETS: Planet[] = [
+  {orbit: 1, name: 'Mercury'},
+  {orbit: 2, name: 'Venus'},
+  {orbit: 3, name: 'Earth'},
+  {orbit: 4, name: 'Mars'},
+  {orbit: 5, name: 'Jupiter'},
+  {orbit: 6, name: 'Saturn'},
+  {orbit: 7, name: 'Uranus'},
+  {orbit: 8, name: 'Neptune'},
+  {orbit: 9, name: 'Pluto'},
+
+];
+
